21c3f92d71c55d7c16f5d450d6554f86dc2b63cf
Mohammed <Mohammed@MOHAMMED-PC>
2026-09-08 07:27:54 +0300

    Rename Properity

--- Program.cs
+++ Program.cs
@@ -29,11 +29,11 @@ await serviceProvider.DisposeAsync();
 var appSettings = configuration.GetSection("AppSettings").Get<AppSettings>();
 Console.WriteLine($"Environment Name: {appSettings.EnvironmentName}");
 Console.WriteLine($"Greeting: {appSettings.Greeting}");
-Console.WriteLine($"Password: {appSettings.Password}");
+Console.WriteLine($"Password: {appSettings.Pasword}");
 
 public sealed class AppSettings
 {
     public string EnvironmentName { get; set; } = string.Empty;
     public string Greeting { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string Pasword { get; set; } = string.Empty;
 }
