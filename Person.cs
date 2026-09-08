b0ee6cbc94a3d60ec88144309aa6d8750cce4368
Mohammed <Mohammed@MOHAMMED-PC>
2026-09-08 11:48:10 +0300

    Remove LICENSE.txt file +  Adding Person Changes

--- Person.cs
+++ Person.cs
@@ -3,10 +3,10 @@ public class Person
     
 public int Age { get; set; }
 
-public string Name { get; set; } = string.Empty;
+public string FirstName { get; set; } = string.Empty;
 
 public int ID  { get; set; }
 
-
+public string LastName { get; set; } = string.Empty;
 
 }
